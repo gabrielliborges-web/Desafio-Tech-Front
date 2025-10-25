b2731c610dacb3116c22d87d14df26a75cbb2b84
gabrielli.melo <gabrielli.melo@simbiox.com.br>
2025-10-25 02:16:26 -0300

    feat(header): integra botão de alternância com persistência do modo dark/light
    - Conexão do Header com o hook useTheme
    - Adicionado botão de alternância com ícones animados
    - Transição suave entre temas utilizando classes do Tailwind

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,39 +1,48 @@
-import { useEffect, useState } from "react";
 import { Sun, Moon } from "lucide-react";
 import Button from "../common/Button";
-import Logo from "../../assets/Title.png";
+import LogoSm from "../../assets/LogoSm.png";
+import LogoLg from "../../assets/LogoLg.png";
+import { useTheme } from "../../hooks/useTheme";
 
 export default function Header() {
-    const [isDark, setIsDark] = useState(false);
-
-
-    useEffect(() => {
-        const root = document.documentElement;
-        if (isDark) root.classList.add("dark");
-        else root.classList.remove("dark");
-    }, [isDark]);
+    const { isDark, toggleTheme } = useTheme();
 
     return (
         <header className="w-full h-[72px] bg-background-dark border-b border-b-border-subtle/20 flex items-center justify-between px-8 font-montserrat transition-colors duration-300">
 
             <div className="flex items-center gap-2">
-                <img src={Logo} alt="Cubos Movies" className="h-[32px]" />
+                <img
+                    src={LogoSm}
+                    alt="Cubos Movies"
+                    className="h-[32px] block md:hidden"
+                />
+
+                <img
+                    src={LogoLg}
+                    alt="Cubos Movies"
+                    className="h-[32px] hidden md:block"
+                />
             </div>
 
             <div className="flex items-center gap-3">
                 <Button
                     variant="icon"
-                    onClick={() => setIsDark((prev) => !prev)}
+                    onClick={toggleTheme}
                     className="relative overflow-hidden transition-all duration-500"
                 >
                     <span
-                        className={`absolute inset-0 flex items-center justify-center transition-all duration-500 ${isDark ? "opacity-0 rotate-90 scale-0" : "opacity-100 rotate-0 scale-100"
+                        className={`absolute inset-0 flex items-center justify-center transition-all duration-500 ${isDark
+                            ? "opacity-0 rotate-90 scale-0"
+                            : "opacity-100 rotate-0 scale-100"
                             }`}
                     >
                         <Sun className="w-5 h-5 text-white" />
                     </span>
+
                     <span
-                        className={`absolute inset-0 flex items-center justify-center transition-all duration-500 ${isDark ? "opacity-100 rotate-0 scale-100" : "opacity-0 -rotate-90 scale-0"
+                        className={`absolute inset-0 flex items-center justify-center transition-all duration-500 ${isDark
+                            ? "opacity-100 rotate-0 scale-100"
+                            : "opacity-0 -rotate-90 scale-0"
                             }`}
                     >
                         <Moon className="w-5 h-5 text-white" />
