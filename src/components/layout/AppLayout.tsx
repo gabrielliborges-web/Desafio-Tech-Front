b4ffbb89709413529d23dc756c7161c7079818fb
gabrielli.melo <gabrielli.melo@simbiox.com.br>
2025-10-25 02:24:45 -0300

    feat(layout): aplica sistema de tema dark/light ao layout principal

--- src/components/layout/AppLayout.tsx
+++ src/components/layout/AppLayout.tsx
@@ -3,10 +3,11 @@ import Footer from "./Footer";
 
 export default function AppLayout({ children }: { children: React.ReactNode }) {
     return (
-        <div className="flex flex-col min-h-screen bg-neutral-950 text-white">
+        <div className="flex flex-col min-h-screen bg-background-light dark:bg-background-dark text-text-primary-light dark:text-text-primary-dark transition-colors duration-300">
             <Header />
             <main className="flex-1 px-6 py-8">{children}</main>
             <Footer />
         </div>
+
     );
 }
