fd68ec5f2a8c90571e220c9c0b9d4344430dca4d
gabrielli.melo <gabrielli.melo@simbiox.com.br>
2025-10-25 13:56:17 -0300

    feat(components): adicionado componente Modal com estrutura dinâmica e transições suaves
    - Criação do componente Modal com header, content e footer dinâmicos
    - Suporte a abertura e fechamento controlado via prop 'open'

--- src/components/movies/MovieActions.tsx
+++ src/components/movies/MovieActions.tsx
@@ -1,14 +1,26 @@
+import { useState } from "react";
 import Input from "../common/Input";
 import Button from "../common/Button";
 import { Search } from "lucide-react";
+import { handleChangeInput } from "../../utils/handleChangeInput";
+import Modal from "../common/Modal";
 
 export default function MovieActions() {
+    const [filters, setFilters] = useState({
+        search: "",
+    });
+
+    const [open, setOpen] = useState(false);
+
     return (
         <section className="w-full flex flex-col gap-3 md:flex-row md:items-center md:justify-end md:gap-3">
             <div className="w-full md:w-[520px] md:flex-shrink-0">
                 <Input
+                    name="search"
                     placeholder="Pesquise por filmes"
                     label=""
+                    value={filters.search}
+                    onChange={(e) => handleChangeInput(e, setFilters)}
                     icon={<Search className="w-5 h-5" />}
                 />
             </div>
@@ -17,6 +29,7 @@ export default function MovieActions() {
                 <Button
                     variant="secondary"
                     className="w-[40%] md:w-auto h-[48px] flex items-center justify-center"
+                    onClick={() => setOpen(true)}
                 >
                     Filtros
                 </Button>
@@ -27,6 +40,34 @@ export default function MovieActions() {
                     Adicionar Filme
                 </Button>
             </div>
+
+            <Modal
+                title="Filtros"
+                open={open}
+                onClose={() => setOpen(false)}
+                footer={
+                    <>
+                        <Button variant="secondary" onClick={() => setOpen(false)}>
+                            Cancelar
+                        </Button>
+                        <Button variant="primary">Aplicar Filtros</Button>
+                    </>
+                }
+            >
+                <div className="border border-red-600/40 rounded-md p-4 text-sm">
+                    <p className="text-center mb-2 font-medium text-yellow-400">
+                        ⚠️ Substitua essa seção pelos filtros ⚠️
+                    </p>
+                    <p className="mb-2">
+                        Este espaço é designado para os filtros que podem ser definidos para
+                        facilitar a pesquisa por um filme específico.
+                    </p>
+                    <p>
+                        É importante ressaltar que o design do formulário deve estar em
+                        harmonia com a identidade visual do restante do site.
+                    </p>
+                </div>
+            </Modal>
         </section>
     );
 }
